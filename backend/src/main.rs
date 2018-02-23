707b1403b468e97305babdae70a2399c6d1cb478
Jonathan Pavlik <jonathan.pavlik@host-studentw-165-112.dhcp.stevens-tech.edu>
2018-02-23 15:33:31 -0500

    rss feed updates and learning rust

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,56 @@
+extern crate regex;
+extern crate reqwest;
+extern crate scraper;
+#[macro_use]
+extern crate serde_derive;
+extern crate toml;
+
+use std::fs::File;
+use std::io::{Read};
+use regex::Regex;
+use scraper::{Html};
+
+#[derive(Deserialize)]
+struct Config {
+  feeds: Vec<Feed>
+}
+
+#[derive(Deserialize)]
+struct Feed {
+  url: String,
+  regex: String,
+  tracker: String,
+}
+
+fn read_config() -> Config {
+  let mut conf = File::open("Config.toml").expect("Error opening config");
+  let mut buf = Vec::new();
+  conf.read_to_end(&mut buf).expect("Error reading config");
+
+  return toml::from_str(String::from_utf8(buf).unwrap().as_ref()).expect("Invalid Config Format");
+}
+
+fn open_url(url: &str) -> Result<Html, reqwest::Error> {
+  Ok(Html::parse_document(get_req(url)?.as_ref()))
+}
+
+fn get_req(uri: &str) -> Result<String, reqwest::Error> {
+  Ok(reqwest::get(uri)?.text()?)
+}
+
 fn main() {
-    println!("Hello, world!");
+  let conf: Config = read_config();
+
+  for feed in &conf {
+    let index_regex = Regex::new(conf.feed.regex.as_ref()).expect("Invalid feed regex");
+
+    loop {
+      
+      
+      
+    }
+    
+  }
+  
+  println!("{} {}", conf.feeds[0].url, index_regex);
 }
