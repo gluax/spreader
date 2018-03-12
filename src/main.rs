876b741db42faccbb11d00b950e7550615c4ec24
Jonathan Pavlik <jonathanpavlik@ip-192-168-1-5.ec2.internal>
2018-03-12 00:35:49 -0400

    on the way to not hardcoded stuff

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,21 @@ extern crate chrono;
 extern crate regex;
 extern crate reqwest;
 extern crate rss;
-extern crate scraper;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate toml;
+extern crate select;
 
+use select::document::Document;
+use select::predicate::{Attr, Class, Name, Predicate};
 use chrono::prelude::*;
 use std::fs::File;
 use std::io::{Read, Write};
 use regex::Regex;
 use rss::Channel;
-use scraper::{Html, Selector};
-use serde::Deserialize;
-use serde::de::Deserializer;
+//use serde::Deserialize;
+//use serde::de::Deserializer;
 
 #[derive(Deserialize, Debug)]
 struct Config {
@@ -31,56 +32,54 @@ struct Feed {
     task: Vec<Task>,
 }
 
-#[derive(Clone, Deserialize, Debug)]
-enum ActionType {
-    ReadFeed,
-    Open,
-    Find,
-    Filter,
-    Get,
-    ToFileFormat,
-    WriteToFile,
+// #[derive(Debug)]
+// enum ActionType {
+//     ReadFeed,
+//     Open,
+//     Find,
+//     Filter,
+//     Get,
+//     ToFileFormat,
+//     WriteToFile,
+// }
+
+#[derive(Clone, Debug)]
+enum TaskType {
+    Dom(select::document::Document),
+    Feed(rss::Channel),
+    Text(String),
 }
 
-fn deserialize_action<'de, D>(deserializer: D) -> Result<ActionType, D::Error> where D: Deserializer<'de> {
-    let s = String::deserialize(deserializer)?;
+impl TaskType {
+    fn dom(self) -> Option<select::document::Document> {
+        if let TaskType::Dom(d) = self {
+            Some(d)
+        } else {
+            None
+        }
+    }
 
-    match s.as_ref() {
-        "read feed" => Ok(ActionType::ReadFeed),
-        "open" => Ok(ActionType::Open),
-        "find" => Ok(ActionType::Find),
-        "filter" => Ok(ActionType::Filter),
-        "get" => Ok(ActionType::Get),
-        "format" => Ok(ActionType::ToFileFormat),
-        "write" => Ok(ActionType::WriteToFile),
-        _ => Err(serde::de::Error::custom("Error trying to deserialize Action policy config"))
+    fn feed(self) -> Option<rss::Channel> {
+        if let TaskType::Feed(f) = self {
+            Some(f)
+        } else {
+            None
+        }
+    }
+
+    fn text(self) -> Option<String> {
+        if let TaskType::Text(t) = self {
+            Some(t)
+        } else {
+            None
+        }
     }
 }
 
 #[derive(Clone, Deserialize, Debug)]
-enum TaskType {
-    Dom(String),
-    File(String),
-    Text(String),
-}
-
-// fn deserialize_tasktype<'de, D>(deserializer: D) -> Result<TaskType, D::Error> where D: Deserializer<'de> {
-//     let s = String::deserialize(deserializer)?;
-
-//     match s.as_ref() {
-//         "dom" => Ok(TaskType::Dom),
-//         "file" => Ok(TaskType::File),
-//         "text" => Ok(TaskType::Text),
-//         _ => Err(serde::de::Error::custom("Error trying to deserialize TaskType policy config"))
-//     }
-// }
-
-#[derive(Clone, Deserialize, Debug)]
 struct Task {
-    //#[serde(deserialize_with="deserialize_action")]
     name: String,
-    //#[serde(deserialize_with="deserialize_tasktype")]
-    task_type: String, //turn this into an enum
+    task_type: String,
     selector: Option<String>,
     selector_attr: Option<String>,
     selector_body: Option<bool>,
@@ -91,8 +90,25 @@ struct Task {
 }
 
 impl Task {
-    fn perform(&self, data: TaskType) -> TaskType {
-        TaskType::Dom("cry".to_string())
+    fn perform(&self, output_path: &str, data: Vec<TaskType>, add: Option<Vec<TaskType>>) -> (Vec<TaskType>, Option<Vec<TaskType>>) {
+        let mut rsp: Vec<TaskType> = Vec::new();
+        let mut additional: Vec<TaskType> = Vec::new();
+
+        //for now all tasktypes are hardcoded will fix in future
+        if self.name.contains("read feed") {
+            rsp = read_feed(data.clone(), "feeds/last_de");
+        }
+        if self.name.contains("get") {
+            additional = get(data.clone(), &self.clone().match_filename.unwrap());
+        }
+        if self.name.contains("to file format") {
+            rsp = file_format(data.clone(), &self.selector.clone().unwrap(), self.selector_body.clone().unwrap(), &self.output_concat.clone().unwrap())
+        }
+        if self.name.contains("open") {
+            rsp = open(data.clone(), self.open_url.unwrap())
+        }
+
+        (rsp, Some(additional))
     }
 }
 
@@ -104,8 +120,8 @@ fn read_config() -> Config {
     toml::from_str(String::from_utf8(buf).unwrap().as_ref()).expect("Invalid Config Format")
 }
 
-fn open_url(url: &str) -> Result<Html, reqwest::Error> {
-    Ok(Html::parse_document(get_req(url)?.as_ref()))
+fn open_url(url: &str) -> Result<select::document::Document, reqwest::Error> {
+    Ok(Document::from(get_req(url)?.as_ref()))
 }
 
 fn get_req(uri: &str) -> Result<String, reqwest::Error> {
@@ -135,92 +151,106 @@ fn read_tracker(path: &str) -> String {
     }
 }
 
-// so maybe a list of links or dom or etc
-// data the type of what's being passed from previous stuff, task the current task, the list of tasks to still be performed
-fn perform_task(data: TaskType, task: &Task, tasks: &mut Vec<Task>) {
-    //somehow do the thing to make it do the right thing
-    // task.name tells us what action to do open, filter, write, etc
-    
-}
-
-fn read_feed(url: &str, output_path: &str, tracker_path: &str, tasks: &mut Vec<Task>) {
-    let (_, tasks) = tasks.split_at_mut(1);
+fn read_feed(data: Vec<TaskType>, tracker_path: &str) -> Vec<TaskType>{
     
     //read last date from tracker file
     let last_update = DateTime::parse_from_rfc2822(&read_tracker(tracker_path)).unwrap().with_timezone(&FixedOffset::east(0));
     
     //read the feed url content
-    let feed_content = Channel::from_url(url).unwrap();
+    let feed_content = data[0].clone().feed().unwrap();
     
     //for each item in feed see if the date of the chapters are greater than our date
+
+    let mut urls: Vec<TaskType> = Vec::new();
     for channel in feed_content.items() {
-        //grab latest chapter as date
         let chapter_pub_date = DateTime::parse_from_rfc2822(channel.pub_date().unwrap()).unwrap();
         //if latest chapter date is newer 
         if chapter_pub_date > last_update {
-            //perform next task on url || return list of them? 
+            //perform next task on url || return list of them?
+            urls.push(TaskType::Text(channel.link().unwrap().to_string()));
             println!("pubDate: {:?}, Chapter: {}", chapter_pub_date, channel.title().unwrap());
-            //open_chap_home(&channel.link().unwrap(), output_path, &mut tasks.to_vec());
         } 
     }
-    
+
+    urls
 }
 
-fn open_chap_home(url: &str, output_path: &str, tasks: &mut Vec<Task>)  {
-    let (task, tasks) = tasks.split_at_mut(1);
-    
-    if task[0].open_url.unwrap() {
-        find_chap_urls(output_path, open_url(url).unwrap(), &mut tasks.to_vec());
-    }
-}
-
-fn find_chap_urls(output_path: &str, dom: scraper::Html, tasks: &mut Vec<Task>) {
-    let (task, tasks) = tasks.split_at_mut(1);
-    let selector = Selector::parse(&task[0].clone().selector.unwrap()).unwrap();
-    
-    for e in dom.select(&selector) {
-        filter_urls(e.value().attr(&task[0].clone().selector_attr.unwrap()).unwrap(), output_path, &mut tasks.to_vec());
-    }
-}
-
-fn filter_urls(url: &str, output_path: &str, tasks: &mut Vec<Task>) {
-    let (task, tasks) = tasks.split_at_mut(1);
-    let filter = Regex::new(&task[0].clone().filter.unwrap()).unwrap();
-
-    if filter.is_match(url) {
-        get_filename_and_open_link(url, output_path, &mut tasks.to_vec());
-    }
-}
-
-fn get_filename_and_open_link(url: &str, output_path: &str, tasks: &mut Vec<Task>) {
-    let (task, _) = tasks.split_at_mut(1);
-    let match_filename = Regex::new(&task[0].clone().match_filename.unwrap()).unwrap();
-    
-    let mat = match_filename.find(url).unwrap();
-    let filename = &url[mat.start()..];
-    
-    let dom = open_url(url).unwrap();
-    get_chapter_content_to_file_format(filename, output_path, dom);
-}
-
-fn get_chapter_content_to_file_format(filename: &str, output_path: &str, dom: scraper::Html) {
-
-    let selector = Selector::parse("div[itemprop=\"articleBody\"] p").unwrap();
-    let mut chapter_paras: Vec<String> = Vec::new();
-    
-    for content in dom.select(&selector) {
-        chapter_paras.push(content.inner_html());
+fn get(data: Vec<TaskType>, matchers: &str) -> Vec<TaskType> {
+    let mut filenames: Vec<TaskType> = Vec::new();
+    let matcher = Regex::new(matchers).unwrap();
+    for ttype in data {
+        let link = ttype.clone().text().unwrap();
+        let matched = matcher.find(&link).unwrap();
+        let filename = link[matched.start()..].to_string();
+        filenames.push(TaskType::Text(filename));
     }
 
+    filenames
+}
+
+fn open(data: Vec<TaskType>, open: bool) -> Vec<TaskType> {
+    let mut doms: Vec<TaskType> = Vec::new();
+    
+    for link in data {
+        if open {
+            let dom = open_url(&link.text().unwrap()).unwrap();
+            //println!("dom: {:?}", dom);
+            doms.push(TaskType::Dom(dom));
+        }
+    }
+    
+    doms
+}
+
+fn file_format(data: Vec<TaskType>, selector: &str, selector_body: bool, output_concat: &str) -> Vec<TaskType> {
+    let mut chapters: Vec<TaskType> = Vec::new();
+
+    let mut chapter = "".to_owned();
+    let concat = output_concat.to_owned();
+    
+    for wdom in data {
+       
+        if selector_body {
+            
+            let dom = wdom.clone().dom().unwrap();
+            // temp harded coded till we write a parser for selector
+            for node in dom.find(Class("section-content").descendant(Name("p"))) {
+                chapter.push_str(&node.text().to_owned());
+                chapter.push_str(&concat);
+            }
+
+            chapters.push(TaskType::Text(chapter.clone()));
+            chapter.clear();
+        }
+    }
+
+    chapters
+}
+
+fn write(output_path: &str, data: Vec<TaskType>, add: Vec<TaskType>) {
+    
 }
 
 fn main() {
     let conf: Config = read_config();
     for feed in &conf.feed {
-        let mut tasks = feed.task.clone();
+        let mut data: Vec<TaskType> = vec![TaskType::Feed(Channel::from_url(&feed.feed_url).unwrap())];
+        let mut add: Option<Vec<TaskType>> = None;
+        //let mut tasks = feed.task.clone();
+        //let mut s: &str = &get_req("http://www.wuxiaworld.com/novel/desolate-era/de-book-42-chapter-20").unwrap();
+        //let dom = Document::from(s);
+        //for node in dom.find(Class("section-content").descendant(Name("p"))) {
+            //println!("text: {}", node.text());
+        //}
+        
         println!("feed: {:?}", feed);
-        feed.task[0].perform(TaskType::Text(feed.feed_url.clone()));
-        read_feed(feed.feed_url.as_ref(), feed.output_path.as_ref(), feed.tracker.as_ref(), &mut tasks);
+        
+        for task in feed.task.clone() {
+            println!("task: {:?}", task);
+            let rsp = task.perform(&feed.output_path.clone(), data.clone(), add.clone());
+            data = rsp.0;
+            add = rsp.1;
+        }
     }
 
 }
